588baa946cfe3677c58c6f5220eb28202f6ad456
pranav <pranavbmaske@gmail.com>
2024-12-16 22:41:46 +0530

    leetcode solutions

--- 9_Pallindrome.js
+++ 9_Pallindrome.js
@@ -6,6 +6,17 @@ var isPalindrome = function(x) {
     return reverse == x;
 };
 
+var isPalindrome1 = function (x) {
+    if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+
+    let reversedHalf = 0;
+    while (x > reversedHalf) {
+        reversedHalf = reversedHalf * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+    return x === reversedHalf || x === Math.floor(reversedHalf / 10);
+};
+
 let main = function() {
 
     let num = 121;
