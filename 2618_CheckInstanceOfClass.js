5fda6cc025ec729bfe369e73b4c5b9d8d3b15019
pranav <pranavbmaske@gmail.com>
2024-12-29 11:14:58 +0530

    leetcode solutions

---
+++ 2618_CheckInstanceOfClass.js
@@ -0,0 +1,21 @@
+
+
+var checkIfInstanceOf = function(obj, classFunction) 
+{
+    while(obj != null)    
+    {
+        if(obj.constructor === classFunction)
+        {
+            return true;
+        }
+        obj = Object.getPrototypeOf(obj);
+    }
+    return false;
+};
+
+let main = function() 
+{
+    
+}
+
+main();
