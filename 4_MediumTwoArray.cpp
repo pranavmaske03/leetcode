28d3247ffcabb0ea64427926265a2bdcfe4c2774
pranav <pranavbmaske@gmail.com>
2025-01-28 17:10:16 +0530

    leetcode solutions

--- 4_MediumTwoArray.cpp
+++ 4_MediumTwoArray.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<vector>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 class Solution {
@@ -7,12 +9,27 @@ class Solution {
         double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
         {
             double medium = 0.0;
-            vector<int> vrr;
-                
+
+            nums1.insert(nums1.end(), nums2.begin(), nums2.end());  
+            sort(nums1.begin(),nums1.end());
+
+            if(nums1.size() % 2 != 0) {
+                int index = nums1.size() / 2;
+                medium = double(nums1[index]);
+            } else {
+                int index = nums1.size() / 2;
+                medium = nums1[index-1] + nums1[index];
+                medium = medium / 2;    
+            }
+            return medium;
         }
 };
 
 int main()
 {
-
+    vector<int> nums1 = {1,2};
+    vector<int> nums2 = {3,4};
+    Solution obj;
+    double ret = obj.findMedianSortedArrays(nums1,nums2);
+    cout<<(ret)<<endl;
 }
