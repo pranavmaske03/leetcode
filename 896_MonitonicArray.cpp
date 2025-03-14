a68aa873188d4765a7214b8b7d966d1d25152f1e
pranav <pranavbmaske@gmail.com>
2025-03-14 10:33:02 +0530

    leetcode solutions

---
+++ 896_MonitonicArray.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+    public:
+        bool isMonotonic(vector<int>& nums) {
+            bool asc = false, desc = false;
+
+            for(int i = 1; i < nums.size(); i++)
+            {
+                if(nums[i-1]<nums[i]) {
+
+                    asc = true;
+                }
+                if(nums[i-1] > nums[i]) {
+                    desc = true;
+                }
+                if(asc && desc) {
+                    return false;
+                }
+            }
+            return true;
+        }
+};
+
+int main() {
+
+
+    return 0;
+}
