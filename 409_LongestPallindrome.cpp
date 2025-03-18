9a3d1e43141f61f32edd0dc84e038f205479eb31
pranav <pranavbmaske@gmail.com>
2025-03-18 09:09:41 +0530

    leetcode solutions

---
+++ 409_LongestPallindrome.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+    public:
+        int longestPalindrome(string s) 
+        {
+            int oddCount = 0;
+            unordered_map<char, int> ump;
+
+            for(char ch : s) {
+                ump[ch]++;
+                if (ump[ch] % 2 == 1) {
+                    oddCount++;
+                } else {
+                    oddCount--;
+                }
+            }
+            if (oddCount > 1) {
+                return s.length() - oddCount + 1;
+            }
+            return s.length();
+        }
+};
+
+int main() 
+{
+
+    return 0;
+}
