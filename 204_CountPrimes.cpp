02b13e50fd1d7cb582df9350c50339a712c0f6f7
pranav <pranavbmaske@gmail.com>
2025-03-29 19:47:21 +0530

    leetcode solutions

---
+++ 204_CountPrimes.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution 
+{
+    public:
+        int countPrimes(int n) {
+            int primeCount = 0;
+            vector<bool> check(n+1, true);
+
+            for(int i = 2; i < n; i++) {
+                if(check[i] == true) {
+                    primeCount++;
+                    for(long long j = (long long)i*i; j < n; j += i) {
+                        check[j] = false;
+                    }
+                }
+            }
+            return primeCount;
+        }
+};
+
+int main() 
+{
+
+    return 0;
+}
