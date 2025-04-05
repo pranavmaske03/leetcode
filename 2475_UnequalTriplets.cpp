1b98fbe456548f9fb246603073207a83dffd558f
pranav <pranavbmaske@gmail.com>
2025-04-05 10:54:12 +0530

    leetcode solutions

---
+++ 2475_UnequalTriplets.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int unequalTriplets(vector<int>& nums) {
+        int count = 0, prev = 0, nxt = nums.size();
+        unordered_map<int, int> frequency;
+
+        for (int n : nums) 
+            frequency[n]++;
+        
+        for (auto[n, freq] : frequency)
+        {
+            nxt -= freq;
+            count += prev * freq * nxt;
+            prev += freq;
+        }
+        return count;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
