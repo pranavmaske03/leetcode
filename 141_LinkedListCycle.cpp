a9bc559fd5957c5249fac94784623bfd80eac78d
pranav <pranavbmaske@gmail.com>
2025-04-28 21:25:25 +0530

    leetcode solutions

--- 141_LinkedListCycle.cpp
+++ 141_LinkedListCycle.cpp
@@ -1,1 +1,35 @@
-141_LinkedListCycle.cpp
+#include<iostream>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+class Solution {
+    public:
+        bool hasCycle(ListNode *head) 
+        {
+            ListNode *_slow = head, *_fast = head;
+
+            while(_slow != NULL) { 
+                if(_fast == NULL || _fast->next == NULL) return false;
+
+                _slow = _slow->next;
+                _fast = _fast->next->next;
+
+                if(_slow == _fast) return true;
+            }
+            return false;
+        }
+};
+
+int main()
+{
+
+    return 0;
+}
