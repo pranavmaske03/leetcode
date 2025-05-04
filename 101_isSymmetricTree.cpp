166406e3e537bdc6417237175230018d320fb8b7
pranav <pranavbmaske@gmail.com>
2025-05-04 12:08:03 +0530

    leetcode solutions

---
+++ 101_isSymmetricTree.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+    public:
+        bool isMirror(TreeNode *root1,TreeNode *root2) {
+
+            if((root1 == nullptr && root2 != nullptr) || (root1 != nullptr && root2 == nullptr)) { 
+                return false;
+            }
+            if(!root1 && !root2) {
+                return true;
+            }
+            if(root1->val != root2->val) {
+                return false;
+            }
+
+            return isMirror(root1->left,root2->right) && isMirror(root1->right,root2->left);
+        }
+
+        bool isSymmetric(TreeNode* root) 
+        {
+            if(!root) return true;
+            return isMirror(root->left,root->right);
+        }
+};
+
+int main()
+{
+
+    return 0;
+}
