1b7aef23259a6b07ddc59e9218c3ff8eaed1e95f
pranavmaske03 <pranavbmaske@gmail.com>
2025-08-19 18:01:35 +0530

    leetcode solutions

--- 1_TwoSum.cpp
+++ 1_TwoSum.cpp
@@ -46,6 +46,20 @@ class Solution
             }
             return ret;
         }
+        vector<int> twoSum1(vector<int>& nums, int target)
+        {
+            unordered_map<int,int> mp;
+
+            for(int i = 0; i < nums.size(); i++)
+            {
+                int complement = target - nums[i];
+                if(mp.count(complement)) {
+                    return {mp[complement], i};
+                } 
+                mp[nums[i]] = i;
+            }
+            return {};
+        }
 };
 
 int main()
