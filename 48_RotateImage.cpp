8e4701839c765a00a6fe15d71c8e332262e18cc6
pranavmaske03 <pranavbmaske@gmail.com>
2025-10-06 23:29:52 +0530

    leetcode solutions

--- 48_RotateImage.cpp
+++ 48_RotateImage.cpp
@@ -2,8 +2,10 @@
 #include<vector>
 
 using namespace std;
-class Solution {
+class Solution 
+{
     public:
+        // transpose and reverse approach
         void rotate(vector<vector<int>>& matrix) 
         {
             for(int i = 0; i < matrix.size()-1; i++)
@@ -16,6 +18,30 @@ class Solution {
             for(int i = 0; i < matrix.size(); i++)
             {
                 reverse(matrix[i].begin(),matrix[i].end());
+            }
+        }
+
+        // layer by layer approach
+        void rotate1(vector<vector<int>>& matrix) 
+        {
+            int n = matrix.size();
+
+            // (left → top → right → bottom → left,) ex. ([0,0] -> [0,2] -> [2,2] -> [2,0] -> [0,0])
+            for(int layer = 0; layer < n/2; layer++) 
+            {
+                int first = layer;
+                int last = n - 1 - layer;
+
+                for(int i = first; i < last; i++)
+                {
+                    int offset = i - first;
+
+                    int top = matrix[first][i]; // store top
+                    matrix[first][i] = matrix[last - offset][first];   //  left -> top
+                    matrix[last - offset][first] = matrix[last][last - offset]; //  bottom -> left
+                    matrix[last][last - offset] = matrix[i][last]; // right -> bottom
+                    matrix[i][last] = top; // top -> right
+                }
             }
         }
 };
