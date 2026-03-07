2f63028ff6fa6b652362c6372071d6b8b6ab3ca3
pranavmaske03 <pranavbmaske@gmail.com>
2026-03-07 23:49:46 +0530

    leetcode solutions

--- 5_LongestPallindrome.cpp
+++ 5_LongestPallindrome.cpp
@@ -27,6 +27,39 @@ class Solution
             }
             return ans;
         }
+    //Manacher's Algorithm
+        string longestPalindrome(string s) {
+        string t = "#";
+        for(char c : s){
+            t += c;
+            t += "#";
+        }
+
+        int n = t.size();
+        vector<int> p(n,0);
+        int center = 0, right = 0;
+        int maxLen = 0, maxCenter = 0;
+
+        for(int i=0;i<n;i++){
+            int mirror = 2*center - i;
+
+            if(i < right)
+                p[i] = min(right - i, p[mirror]);
+            while(i - p[i] - 1 >= 0 && i + p[i] + 1 < n && t[i - p[i] - 1] == t[i + p[i] + 1])
+                p[i]++;
+
+            if(i + p[i] > right){
+                center = i;
+                right = i + p[i];
+            }
+            if(p[i] > maxLen){
+                maxLen = p[i];
+                maxCenter = i;
+            }
+        }
+        int start = (maxCenter - maxLen) / 2;
+        return s.substr(start, maxLen);
+    }
 };
 
 int main()
