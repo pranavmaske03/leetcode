5de49566a69bd7577db08ad8f6dc5a16e4697d73
pranavmaske03 <pranavbmaske@gmail.com>
2026-04-24 12:22:17 +0530

    leetcode solutions

--- 222_CountTreeNodes.cpp
+++ 222_CountTreeNodes.cpp
@@ -1,30 +1,62 @@
 #include<iostream>
 
-  // Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}  // default constructor
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {} // parametarised constructor
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {} // also parametarised constructor
-};
-  
- 
-class Solution 
-{
-    public:
-        int countNodes(TreeNode* root) 
-        {
-            if(root == NULL) {
-                return 0;
-            }
-            return countNodes(root->left) + countNodes(root->right) + 1;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int getLeftHeight(TreeNode* node) {
+        int h = 0;
+        while (node) {
+            h++;
+            node = node->left;
         }
+        return h;
+    }
+
+    int getRightHeight(TreeNode* node) {
+        int h = 0;
+        while (node) {
+            h++;
+            node = node->right;
+        }
+        return h;
+    }
+
+    int countNodes(TreeNode* root) {
+        if (root == NULL) return 0;
+
+        int lh = getLeftHeight(root);
+        int rh = getRightHeight(root);
+
+        if (lh == rh) {
+            return (1 << lh) - 1;
+        }
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
 };
 
 int main()
 {
-
+    Solution sol;
+    // Construct a small complete binary tree:
+    //       1
+    //      / \
+    //     2   3
+    //    / \
+    //   4   5
+    TreeNode* root = new TreeNode(1,
+                        new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+                        new TreeNode(3));
+    std::cout << sol.countNodes(root) << std::endl;
     return 0;
 }
