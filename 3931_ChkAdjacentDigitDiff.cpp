fb1f23d741d666b6d350ced584708518c8b713db
pranavmaske03 <pranavbmaske@gmail.com>
2026-05-18 19:02:55 +0530

    leetcode solutions

---
+++ 3931_ChkAdjacentDigitDiff.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        for(int i = 1;i < s.size(); i++){
+            int a = s[i] - '0';
+            int b = s[i-1] - '0';
+            if(abs(a - b) > 2) return false;
+        } 
+        return true;
+    }
+};
