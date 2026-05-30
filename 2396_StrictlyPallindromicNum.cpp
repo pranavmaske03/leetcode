1fdd8aabb5a067f25d98f8d569bf3c70805ad575
pranavmaske03 <pranavbmaske@gmail.com>
2026-05-30 12:39:23 +0530

    leetcode solutions

---
+++ 2396_StrictlyPallindromicNum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+        
+    }
+};
+
+int main() {
+    return 0;
+}
