6c3ee7f174d0a9a7338acbaadb1ab787812af153
pranavmaske03 <pranavbmaske@gmail.com>
2026-06-08 21:14:50 +0530

    leetcode solutions

---
+++ 1929_ConcatenationOfArray.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans(2 * nums.size());
+        int n = nums.size();
+        for(int i = 0; i < nums.size(); i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
