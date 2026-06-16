36d5ea865377491265689bc7db16674e661e3e14
PRANAV MASKE <pranavbmaske@gmail.com>
2026-06-16 20:41:39 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (58.81%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+       string lsp = "";
+
+        for(int i = 0; i < strs[0].size(); i++) {
+            for(int j = 1; j < strs.size(); j++) {
+                if(i >= strs[j].size() or strs[j][i] != strs[0][i]) {
+                    return lsp;
+                }
+            }
+            lsp += strs[0][i];
+        }
+        return lsp;
+    }
+};
