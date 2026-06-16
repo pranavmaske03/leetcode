a082b744e7542fe5cff909a92e73f77811dcaef9
PRANAV MASKE <pranavbmaske@gmail.com>
2026-06-16 22:51:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (33.12%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length = 0;
+        int n = s.length();
+
+        int i = n - 1;
+        while(i >= 0 and s[i] == ' ') i--;
+        while(i >= 0 and s[i] != ' ') length++,i--;
+
+        return length;
+
+    }
+};
