dfda581701e3dfaedb634b52d5da2d6716160d6c
PRANAV MASKE <pranavbmaske@gmail.com>
2026-06-17 14:00:30 +0530

    Time: 4 ms (8.27%) | Memory: 12.5 MB (7.41%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -1,20 +1,26 @@
-class Solution 
-{
-    public:
-        int timeRequiredToBuy(vector<int>& tickets, int k) 
-        {
-            int time = 0;        
-            for (int i = 0; i < tickets.size(); i++) {
-            // If the current person is before or at the desired person 'k'
-                if (i <= k) {
-                    // Buy the minimum of tickets available at person 'k' and the current person
-                    time += min(tickets[k], tickets[i]);
-                } else {
-                    // If the current person is after 'k', buy the minimum of 
-                    // (tickets available at person 'k' - 1) and the current person
-                    time += min(tickets[k] - 1, tickets[i]);
-                }
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        queue<pair<int,int>> q;
+        int time = 0;
+
+        for(int i = 0; i < tickets.size(); i++) {
+            q.push({i, tickets[i]});
+        }
+
+        while(!q.empty()) {
+            auto [idx, rem_tickets] = q.front();
+            q.pop();
+            
+            rem_tickets--;
+            time++;
+
+            if(rem_tickets == 0) {
+                if(idx == k) return time;
+            } else {
+                q.push({idx, rem_tickets});
             }
+        }
         return time;
     }
 };
