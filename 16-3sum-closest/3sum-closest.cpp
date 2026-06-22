e2839cf2410d1f897db4a3143fd003638973a525
PRANAV MASKE <pranavbmaske@gmail.com>
2026-06-22 07:48:33 +0530

    Time: 7 ms (87.77%) | Memory: 14 MB (37.53%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
+        int n = nums.size();
+        int res = nums[0] + nums[1] + nums[2];
+
         sort(nums.begin(), nums.end());
-        int sum = 0;
-        int closest = nums[0] + nums[1] + nums[2];
+        for(int i = 0; i < n - 2; i++) {
+            int j = i + 1, k = n - 1;
 
-        for(int i = 0; i < nums.size(); i++) {
-            int left = i + 1; 
-            int right = nums.size() - 1;
-
-            while(left < right) {
-                sum = nums[i] + nums[left] + nums[right];
+            while(j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                
                 if(sum == target) 
                     return sum;
+                if(abs(sum - target) < abs(res - target))
+                    res = sum;
 
-                if(abs(sum - target) < abs(closest - target)) 
-                    closest = sum;
-
-                if(sum > target)    
-                    right--;
-                else  
-                    left++;
+                if(sum > target) k--;
+                else j++;
             }
         }
-        return closest;
+        return res;
     }
 };
