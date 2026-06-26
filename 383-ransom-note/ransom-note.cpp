1607a7fe062b42f908707dd96b572dc805b1123b
PRANAV MASKE <pranavbmaske@gmail.com>
2026-06-26 13:36:18 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (98.12%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> freq(26, 0);
+
+        for(auto& ch : magazine) {
+            freq[ch - 'a']++;
+        }
+
+        for(auto& ch : ransomNote) {
+            if(freq[ch - 'a']-- <= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
