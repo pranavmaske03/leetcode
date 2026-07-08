5f6c24fb39d0a115542cba36811eae37dbadadb1
PRANAV MASKE <pranavbmaske@gmail.com>
2026-07-08 22:27:01 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (64.32%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -6,12 +6,12 @@ public:
         while(left < right) {
             int mid = left + (right - left) / 2;
 
-            if(nums[mid] > nums[right]) {
-                left = mid + 1;
-            } else if(nums[mid] < nums[right]){
-                right = mid; 
-            } else {
+            if(nums[mid] == nums[right]) {
                 right--;
+            } else if(nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid; 
             }
         }
         return nums[left];
