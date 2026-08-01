d25f853cade730eb9682e92e2a821723a838cc1c
PRANAV MASKE <pranavbmaske@gmail.com>
2026-08-01 17:11:48 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (88.14%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -13,10 +13,24 @@ public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
         vector<bool> visited(n, false);
+        queue<int> q;
 
-        dfs(0, rooms, visited);
-        for(auto val : visited) {
-            if(!val) 
+        q.push(0);
+        visited[0] = true;
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for(int neighbours : rooms[node]) {
+                if(!visited[neighbours]) {
+                    visited[neighbours] = true;
+                    q.push(neighbours);
+                }
+            }
+        }
+        for(bool isVisited : visited) {
+            if(!isVisited) 
                 return false;
         }
         return true;
