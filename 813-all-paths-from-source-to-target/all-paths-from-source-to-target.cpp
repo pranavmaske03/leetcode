422229417245c448c5b70d89705e23f66232b899
PRANAV MASKE <pranavbmaske@gmail.com>
2026-08-02 15:51:56 +0530

    Time: 7 ms (41.30%) | Memory: 15.3 MB (42.99%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    void dfs(int node, vector<vector<int>>& graph, vector<int>& path, vector<bool>& visited, int& dest) {
+
+        if(!visited[node]) {
+            path.push_back(node);
+        }
+        if(node == dest)    {
+            res.push_back(path);
+            path.pop_back();
+            return;
+        }
+
+        visited[node] = true;
+        for(auto& neibour : graph[node]) {
+            if(!visited[neibour]) {
+                dfs(neibour, graph, path, visited, dest);
+            }
+        }
+        path.pop_back();
+        visited[node] = false;
+    }
+
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> path;
+        vector<bool> visited(n, false);
+        int source = 0, dest = n - 1;
+
+        dfs(source, graph, path, visited, dest);
+        return res;
+    }
+};
