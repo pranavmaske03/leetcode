fa7ef0c6a127dbceea100cba06b0e90c5c2ace5d
PRANAV MASKE <pranavbmaske@gmail.com>
2026-08-05 12:15:14 +0530

    Time: 3 ms (42.36%) | Memory: 13 MB (85.22%) - LeetSync

--- 1380-number-of-closed-islands/number-of-closed-islands.cpp
+++ 1380-number-of-closed-islands/number-of-closed-islands.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
-    bool dfs(int row, int col, vector<vector<int>>& grid, vector<vector<bool>>& visited) {
-        int rows = grid.size();
-        int cols = grid[0].size();
+    int rows;
+    int cols;
 
-        if(row < 0 || row >= rows || col < 0 || col >= cols) {
+    bool dfs(int row, int col, vector<vector<int>>& grid) {
+        if (row < 0 || row >= rows || col < 0 || col >= cols) {
             return false;
         }
-        if(grid[row][col] == 1 || visited[row][col]) {
+        if (grid[row][col] == 1) {
             return true;
         }
 
-        visited[row][col] = true;
-        bool down = dfs(row + 1, col, grid, visited);
-        bool up = dfs(row - 1, col, grid, visited);
-        bool right = dfs(row, col + 1, grid, visited);
-        bool left = dfs(row, col - 1, grid, visited);
+        grid[row][col] = 1;
+
+        bool down = dfs(row + 1, col, grid);
+        bool up = dfs(row - 1, col, grid);
+        bool right = dfs(row, col + 1, grid);
+        bool left = dfs(row, col - 1, grid);
 
         return down && up && right && left;
     }
 
     int closedIsland(vector<vector<int>>& grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
+        rows = grid.size();
+        cols = grid[0].size();
 
         int closed = 0;
-        bool isClosed = false;
-        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
-        for(int row = 0; row < rows; row++) {
-            for(int col = 0; col < cols; col++) {
-                if(grid[row][col] == 0 && !visited[row][col]) {
-                    if(dfs(row, col, grid, visited))
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (grid[row][col] == 0) {
+                    if (dfs(row, col, grid)) {
                         closed++;
+                    }
                 }
             }
         }
