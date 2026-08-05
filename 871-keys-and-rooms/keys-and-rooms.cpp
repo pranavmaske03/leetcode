60b9588994616d9e7e99c66bbc74ac70e51585c6
PRANAV MASKE <pranavbmaske@gmail.com>
2026-08-05 15:13:32 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (72.47%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,15 +1,5 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& graph, vector<bool>& visited) {
-        visited[node] = true;
-
-        for(auto& neighbours : graph[node]) {
-            if(!visited[neighbours]) {
-                dfs(neighbours, graph, visited);
-            }
-        }
-    }
-
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
         vector<bool> visited(n, false);
