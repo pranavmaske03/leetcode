0567fea547a501708e6028540989569f90454923
PRANAV MASKE <pranavbmaske@gmail.com>
2026-08-06 18:23:19 +0530

    Time: 99 ms (83.71%) | Memory: 126.5 MB (60.14%) - LeetSync

---
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dfs(int node, vector<vector<int>>& graph, vector<int>& informTime) {
+        if(informTime[node] == 0)
+            return 0;
+
+        int time = 0;
+        for(auto& neibour : graph[node]) {
+            time = max(time, dfs(neibour, graph, informTime));
+        }
+
+        return informTime[node] + time;
+    }
+
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        vector<vector<int>> graph(n);
+
+        for(int i = 0; i < manager.size(); i++) {
+            if(manager[i] != -1) {
+                graph[manager[i]].push_back(i);
+            }
+        }
+        return dfs(headID, graph, informTime);
+    }
+};
