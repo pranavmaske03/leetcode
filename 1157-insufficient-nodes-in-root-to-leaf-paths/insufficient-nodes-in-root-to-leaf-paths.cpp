6634ab44ad44bf8fd45ffe0993ee27f270528caf
PRANAV MASKE <pranavbmaske@gmail.com>
2026-08-11 22:22:16 +0530

    Time: 0 ms (100.00%) | Memory: 34.4 MB (94.26%) - LeetSync

--- 1157-insufficient-nodes-in-root-to-leaf-paths/insufficient-nodes-in-root-to-leaf-paths.cpp
+++ 1157-insufficient-nodes-in-root-to-leaf-paths/insufficient-nodes-in-root-to-leaf-paths.cpp
@@ -11,28 +11,19 @@
  */
 class Solution {
 public:
-    TreeNode* DFS(TreeNode* root, int sum, int& limit) {
-        if(!root)
+    TreeNode* sufficientSubset(TreeNode* root, int limit) {
+        if(root == nullptr)
             return nullptr;
         
-        sum += root->val;
-        if(!root->left && !root->right) {
-            if(sum < limit)
-                return nullptr;
-            else
-                return root;
-        }
-        root->left = DFS(root->left, sum, limit);
-        root->right = DFS(root->right, sum, limit);
+        if(!root->left && !root->right)
+            return root->val < limit ? nullptr : root;
+
+        root->left = sufficientSubset(root->left, limit - root->val);
+        root->right = sufficientSubset(root->right, limit - root->val);
 
         if(!root->left && !root->right)
             return nullptr;
-
+        
         return root;
-    }
-
-    TreeNode* sufficientSubset(TreeNode* root, int limit) {
-        int sum = 0;
-        return DFS(root, sum, limit);
     }
 };
