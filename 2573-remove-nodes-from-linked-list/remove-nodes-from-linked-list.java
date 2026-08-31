1ec5f2cb8380e57199b7e7dd5ea7860d6f89aab4
PRANAV MASKE <pranavbmaske@gmail.com>
2026-08-31 16:00:44 +0530

    Time: 71 ms (34.03%) | Memory: 134.6 MB (38.35%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -10,23 +10,22 @@
  */
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        Stack<Integer> stack = new Stack<>();
+        Stack<ListNode> stack = new Stack<>();
 
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        ListNode tmp = head;
-        while(tmp != null) {
-            while(!stack.isEmpty() && stack.peek() < tmp.val) {
+        ListNode curr = head;
+        while(curr != null) {
+            while(!stack.isEmpty() && stack.peek().val < curr.val) {
                 stack.pop();
             }
-            stack.push(tmp.val);
-            tmp = tmp.next;
+            stack.push(curr);
+            curr = curr.next;
         }
+        ListNode prev = null;
         while(!stack.empty()) {
-            ListNode newn = new ListNode(stack.pop());
-            newn.next = curr.next;
-            curr.next = newn;
+            ListNode node = stack.pop();
+            node.next = prev;
+            prev = node;
         }
-        return dummy.next;
+        return prev;
     }
 }
