3a036bf8024d584d24144b1e23f42b6429da9419
PRANAV MASKE <pranavbmaske@gmail.com>
2026-09-16 12:23:09 +0530

    Time: 2 ms (98.83%) | Memory: 42.9 MB (99.81%) - LeetSync

--- 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.java
+++ 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean isValidSerialization(String preorder) {
-        String[] tokens = preorder.split(",");
         int slot = 1;
+        int n = preorder.length();
 
-        for (int i = 0; i < tokens.length; i++) {
-            if (tokens[i].equals("#"))
-                slot--;
-            else
-                slot++;
+        for (int i = 0; i < n; i++) {
+            if(preorder.charAt(i) == ',') {
+                --slot;
+                if(slot < 0)
+                    return false;
 
-            if (slot == 0 && i != tokens.length - 1)
-                return false;
+                if(preorder.charAt(i - 1) != '#') 
+                    slot += 2;
+            }
         }
+        slot = preorder.charAt(n - 1) != '#' ? slot + 1 : slot - 1;
         return slot == 0;
     }
 }
